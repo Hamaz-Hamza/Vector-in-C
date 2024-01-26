6bd15b4aafdf36480d39ee5b5a8e07701cd22db4
Hamaz <135416194+Hamaz-Hamza@users.noreply.github.com>
2024-01-26 19:20:00 +0500

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main() {
         VectorPush(vec, i+1);
         PrintVector(vec);
     }
+    printf("\nIndex of 10: %d\n\n", FindInVector(vec, 10));
     for (int i = 0; i < 20; i++) {
         VectorPop(vec);
         PrintVector(vec);
